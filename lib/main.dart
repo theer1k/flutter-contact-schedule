584e882ba48eaa3cf440fc263b3094df105b4da1
Erik <erik.hac@outlook.com>
2019-07-28 22:56:10 -0300

    Add contact page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:contact_schedule/ui/contact_page.dart';
 import 'package:contact_schedule/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: HomePage(),
+  home: ContactPage(),
 ));
