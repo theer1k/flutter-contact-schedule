9150ccc5f47cd3a8709857a8c877f98cedd1a9df
Erik <erik.hac@outlook.com>
2019-07-28 23:09:12 -0300

    Add navigating through pages with update or save new contact

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:contact_schedule/ui/contact_page.dart';
 import 'package:contact_schedule/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: ContactPage(),
+  home: HomePage(),
 ));
